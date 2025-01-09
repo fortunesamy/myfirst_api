b321b137b96a126bb4ad50a9d5f1ab7871edbe83
fortunesamy <fortunesamy01@gmail.com>
2025-01-09 17:47:22 +0100

    added readme

---
+++ README.md
@@ -0,0 +1,6 @@
+# My First API with Python
+ hshjhjhkhkhjhkjdhcjdhjdhc
+
+
+ ## the FLASK
+ in this projetc i have used Flask for ...
--- app.py
+++ app.py
@@ -4,3 +4,4 @@ from flask_cors import CORS
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required
 from marshmallow import Schema, fields, ValidationError
 from flask_sqlalchemy import SQLAlchemy
+
